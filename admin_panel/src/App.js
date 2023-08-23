a1f270110d7e8c1dfdc7db250a300d2abbd204dd
Miraanz15 <miraanejaz15@gmail.com>
2023-08-24 02:25:18 +0530

    DOM imported

--- admin_panel/src/App.js
+++ admin_panel/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello.
+          Hello World
         </p>
       </header>
     </div>
